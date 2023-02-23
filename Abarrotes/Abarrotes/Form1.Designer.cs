702798343d48aae4ab53468dc1928c198e4ec675
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 21:54:14 -0600

    Add files via upload

---
+++ Abarrotes/Abarrotes/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Abarrotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.ptbFoto = new System.Windows.Forms.PictureBox();
+            this.lblFoto = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnEditarT = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.dgvDato = new System.Windows.Forms.DataGridView();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbFoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDato)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(722, 53);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(50, 13);
+            this.lblProducto.TabIndex = 1;
+            this.lblProducto.Text = "Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Abarrotes";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(701, 212);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 5;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(701, 135);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 7;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(734, 119);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 6;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // ptbFoto
+            // 
+            this.ptbFoto.Location = new System.Drawing.Point(842, 53);
+            this.ptbFoto.Name = "ptbFoto";
+            this.ptbFoto.Size = new System.Drawing.Size(199, 203);
+            this.ptbFoto.TabIndex = 8;
+            this.ptbFoto.TabStop = false;
+            // 
+            // lblFoto
+            // 
+            this.lblFoto.AutoSize = true;
+            this.lblFoto.Location = new System.Drawing.Point(896, 37);
+            this.lblFoto.Name = "lblFoto";
+            this.lblFoto.Size = new System.Drawing.Size(42, 13);
+            this.lblFoto.TabIndex = 9;
+            this.lblFoto.Text = "Imagen";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(734, 252);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(658, 349);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(779, 349);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnEditarT
+            // 
+            this.btnEditarT.Location = new System.Drawing.Point(400, 331);
+            this.btnEditarT.Name = "btnEditarT";
+            this.btnEditarT.Size = new System.Drawing.Size(75, 23);
+            this.btnEditarT.TabIndex = 14;
+            this.btnEditarT.Text = "Editar";
+            this.btnEditarT.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(160, 331);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(722, 196);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblCantidad.TabIndex = 16;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Items.AddRange(new object[] {
+            "Arroz",
+            "Jabón",
+            "Azúcar",
+            "Café",
+            "Cereal",
+            "Avena"});
+            this.cmbProducto.Location = new System.Drawing.Point(690, 69);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbProducto.TabIndex = 17;
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(917, 268);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(75, 23);
+            this.btnExaminar.TabIndex = 18;
+            this.btnExaminar.Text = "Examinar";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(960, 399);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 19;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // dgvDato
+            // 
+            this.dgvDato.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDato.Location = new System.Drawing.Point(106, 35);
+            this.dgvDato.Name = "dgvDato";
+            this.dgvDato.Size = new System.Drawing.Size(445, 104);
+            this.dgvDato.TabIndex = 20;
+            this.dgvDato.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDato_CellContentClick);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(701, 283);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 443);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.dgvDato);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.cmbProducto);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditarT);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblFoto);
+            this.Controls.Add(this.ptbFoto);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblProducto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbFoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDato)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.PictureBox ptbFoto;
+        private System.Windows.Forms.Label lblFoto;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnEditarT;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.Button btnExaminar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView dgvDato;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ Abarrotes/Abarrotes/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Abarrotes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvDato.Columns.Add("producto", "Producto");
+            dgvDato.Columns.Add("precio", "Precio");
+            dgvDato.Columns.Add("cantidad", "Cantidad de Compra");
+            dgvDato.Columns.Add("total", "Total");
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            dgvDato.Rows.Add(cmbProducto.Text, txtPrecio.Text, txtCantidad.Text, txtTotal.Text);
+        }
+
+        private void txtPrecio_TextChanged(object sender, EventArgs e)
+        {
+            if (cmbProducto.Text == "Café")
+            {
+                txtPrecio.Text = "100";
+            }
+            else if (cmbProducto.Text == "Azúcar")
+            {
+                txtPrecio.Text = "28";
+            }
+            else if (cmbProducto.Text == "Jabón")
+            {
+                txtPrecio.Text = "38";
+            }
+            else if (cmbProducto.Text == "Arroz")
+            {
+                txtPrecio.Text = "30";
+            }
+            else if (cmbProducto.Text == "Cereal")
+            {
+                txtPrecio.Text = "46";
+            }
+            else if (cmbProducto.Text == "Avena")
+            {
+                txtPrecio.Text = "25";
+            }
+            else if (cmbProducto.Text == null)
+            {
+                txtPrecio.Text = "";
+            }
+
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+            int n1, n2, r;
+            n1 = Convert.ToInt32(txtCantidad.Text);
+            n2 = Convert.ToInt32(txtPrecio.Text);
+            r = n1 * n2;
+            txtTotal.Text = Convert.ToString(r);
+            //txtTotal.Text = r;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Text = "";
+            txtPrecio.Text = "";
+            txtPrecio.Text = "";
+            txtTotal.Text = "";
+            cmbProducto.Text = "";
+        }
+
+        private void dgvDato_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvDato.Rows.Remove(dgvDato.CurrentRow);
+        }
+        
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try {
+                dgvDato.Rows.Remove(dgvDato.CurrentRow);
+            }
+            catch
+            {
+                MessageBox.Show("Error en la ejecución de su solicitud ");
+            }
+        }
+    
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            
+                if (MessageBox.Show("¿Desea Salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    this.Close();
+                    Application.Exit();
+                }
+                else
+                {
+                    this.cmbProducto.Focus();
+                }
+            }
+        
+    }
+    }
